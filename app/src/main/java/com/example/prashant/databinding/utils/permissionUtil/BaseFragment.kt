a5a4d3f1df35d907ea2d8edd2b20e63675811725
Prashant Panwar <prashant.panwar777@gmail.com>
2019-08-16 00:37:40 +0530

    chore: migrate to androidx and material theming

--- app/src/main/java/com/example/prashant/databinding/utils/permissionUtil/BaseFragment.kt
+++ app/src/main/java/com/example/prashant/databinding/utils/permissionUtil/BaseFragment.kt
@@ -1,7 +1,7 @@
 package com.example.prashant.databinding.utils.permissionUtil
 
 import android.content.Context
-import androidx.fragment.app.Fragment
+import android.support.v4.app.Fragment
 
 /**
  * A simple [Fragment] class,
